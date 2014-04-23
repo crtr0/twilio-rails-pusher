ba01b3d9eb86a73f92d0dd81bc0f046b9f5bd7e5
Carter Rabasa <carter.rabasa@gmail.com>
2014-04-23 14:57:40 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwilioRailsPusher::Application.config.secret_key_base = 'c9c12bd2b42bf4421ba75810d5d5ab67a58ab70ba8a6c83d16092e473cd768859e7b79a9a6be89186d00f9cf67217436fc85aa10847aa66b1cef517af9aa0f37'
